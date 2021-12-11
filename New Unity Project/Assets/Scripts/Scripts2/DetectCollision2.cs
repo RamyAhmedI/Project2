401c6c8fd0f3f1d902ced994555f57d55a583ea8
RamyAhmedI <91535329+RamyAhmedI@users.noreply.github.com>
2021-12-11 19:20:27 +0000

    Final Version Patch 3
    Added characters to other 2 levels , Added collisions and other basic required assets

---
+++ New Unity Project/Assets/Scripts/Scripts2/DetectCollision2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision2 : MonoBehaviour
+{
+    public GameObject hero;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(collider == hero){
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        }
+        for(int i = 0; i< 1; i++){
+            Status();
+        }
+    }
+    private void Status(){
+        if(hero == null){
+            Debug.Log("Ded");
+        }
+    }
+}
